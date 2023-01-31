761cfdb6fd7ccfd2dc9010ed79825f78dba9eecf
ericdew <me@ericdew.com>
2023-01-30 23:54:33 -0800

    Apply consistent code styles (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ericdew.com',
+  site: "https://ericdew.com",
   integrations: [
     tailwind(),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp',
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
-  ]
+  ],
 });
