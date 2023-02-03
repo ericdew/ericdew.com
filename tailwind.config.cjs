14106cd71dca6d18066b8d6d2004be2e1e6cd4de
ericdew <me@ericdew.com>
2023-02-03 01:29:33 -0800

    Improve gallery ux

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -23,11 +24,35 @@ module.exports = {
           "80%": { transform: "translate3d(-9rem, 1rem, 0)" },
           "90%": { transform: "translate3d(6rem, -5rem, 0)" },
         },
+        slideOutLeft: {
+          "0%": { left: "0%", opacity: "1" },
+          "100%": { left: "-25%", opacity: "0" },
+        },
+        slideOutRight: {
+          "0%": { left: "0%", opacity: "1" },
+          "100%": { left: "25%", opacity: "0" },
+        },
+        slideInLeft: {
+          "0%": { left: "-25%", opacity: "0" },
+          "100%": { left: "0%", opacity: "1" },
+        },
+        slideInRight: {
+          "0%": { left: "25%", opacity: "0" },
+          "100%": { left: "0%", opacity: "1" },
+        },
       },
       animation: {
         grain: "grain 1s steps(2) infinite",
+        "slide-out-left": "slideOutLeft 200ms ease-in-out",
+        "slide-out-right": "slideOutRight 200ms ease-in-out",
+        "slide-in-left": "slideInLeft 200ms ease-in-out",
+        "slide-in-right": "slideInRight 200ms ease-in-out",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("fullscreen", ["&#fullscreen", "#fullscreen &"]);
+    }),
+  ],
 };
