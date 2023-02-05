17208173d6443c33798c5a696c5b1cc5eb9c0345
ericdew <me@ericdew.com>
2023-02-05 01:45:47 -0800

    Update ga id

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
-  readonly PUBLIC_CLOUDINARY_API_KEY: string;
-  readonly PUBLIC_GA_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
